e1eaaad1771752479ea8fd4826adc8dd5b127203
shihab0952 <md.shihab.uddin123409@gmail.com>
2025-02-23 03:20:20 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,3 @@
-// import { createRoot } from 'react-dom/client';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import './index.css'
-// import App from './App';
-// import Home from './pages/home/Home';
-// import Blog from './pages/home/Blog';
-// import Services from './pages/home/Services';
-// import ErrorPage from './component/ErrorPage';
-// import Pricing from './pages/home/Pricing';
-// import Register from './component/Register';
-
-// createRoot(document.getElementById('root')).render(
-//   <BrowserRouter>
-//     <Routes>
-//       {/* Parent Route */}
-//       <Route path='/' element={<App />}>
-//         {/* Child Routes */}
-//         <Route index element={<Home />} /> {/* Default route */}
-//         <Route path='home' element={<Home />} />
-//         <Route path='blog' element={<Blog />} />
-//         <Route path='services' element={<Services />} />
-//         <Route path='freeTrail' element={<Pricing/>} />
-//         <Route path='*' element={<ErrorPage />} />
-
-//         {/* Authentication */}
-
-//         <Route path='/register' element= {<Register/>}/>
-       
-//       </Route>
-//     </Routes>
-//   </BrowserRouter>
-// );
-
-
-
-
-
 
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -56,9 +19,9 @@ createRoot(document.getElementById('root')).render(
         <Route index element={<Home />} /> {/* Default route */}
         <Route path='home' element={<Home />} />
         <Route path='blog' element={<Blog />} />
-        <Route path='services' element={<Services />} />
-        <Route path='freeTrail' element={<Pricing />} />
-        <Route path='*' element={<ErrorPage />} />
+        <Route path='services' element={<Services/>} />
+        <Route path='freeTrail' element={<Pricing/>} />
+        <Route path='*' element={<ErrorPage/>} />
       </Route>
 
       {/* Register Route (outside App) */}
